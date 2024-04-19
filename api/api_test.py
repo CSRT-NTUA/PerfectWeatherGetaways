43ff1508168eefea5bd7170565e45b9f0171d7c2
Spiros Maggioros <spirosastro@gmail.com>
2024-04-20 00:29:43 +0300

    added weather api

--- api/api_test.py
+++ api/api_test.py
@@ -4,37 +4,43 @@
 import requests
 import openai
 
+
+prompt = "I want to go for sunny holidays in Europe. I want a city with a big cathedral. Use the following instructions when answering the prompt above: Reply in Json format Include the places suggestions in an array. Suggest as many as you can, preferably at least 10. When writing, the place name includes only the name, not the country, wider region or continent. If the prompt is appropriate for any of the following categories include the category in the json: Categories: infinity_pool,heated_pool,indoor_pool,rooftop_pool,wave_pool,children_pool,panoramic_view_pool,pool_swim_up_bar,pool_water_slide,pool_lap_lanes,water_park,lazy_river,private_pool,dog_play_area,dog_sitting,dogs_stay_free,outdoor_pool,health_and_safety,treehouse,haunted,overwater_bungalows,three_star,skyscraper,four_star,five_star,yoga,tennis,small,adult_only,gym,accessible,cheap,parking,business,free_wifi,pool,nightlife,romantic,dog_friendly,family,spa,casino,honeymoon,eco_friendly,beach,beachfront,ski,ski_in_ski_out,historic,unusual,vineyard,monastery,castle,golf,luxury,boutique,ev_charging,jacuzzi_hot_tub,fireplace,all_inclusive"
+
+# response 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {
       "role": "user",
-      "content": "Hello there, can you say hi to me?"
+      "content": prompt,
     }
   ],
-  temperature=0,
+  temperature=2,
   max_tokens=1000,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
+  # top_p=1,
+  # frequency_penalty=0,
+  # presence_penalty=0
 )
-# response["choices"][0]["message"]["content"]
+
+# here we get the json format
+# response = response["choices"][0]["message"]["content"]
 print(response)
 
-url = "https://www.travelmyth.gr/api_chat_makeathon.php"
-params = {
-    "destination": "Greece",
-    "lang": "en",
-    "categories": "ski,luxury",
-    "apiKey": "csrt"
-}
-headers = {"content-type": "application/json"}
-
-response = requests.get(url, params=params, headers=headers)
-
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-    # Process the JSON data here
-else:
-    print("Error:", response.status_code)
+#url = "https://www.travelmyth.gr/api_chat_makeathon.php"
+#params = {
+#    "destination": "Greece",
+#    "lang": "en",
+#    "categories": "ski,luxury",
+#    "apiKey": "csrt"
+#}
+#headers = {"content-type": "application/json"}
+#
+#response = requests.get(url, params=params, headers=headers)
+#
+#if response.status_code == 200:
+#    data = response.json()
+#    print(data)
+#    # Process the JSON data here
+#else:
+#    print("Error:", response.status_code)
---
+++ api/meteo_forecast.py
@@ -0,0 +1,44 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"hourly": "temperature_2m",
+	"forecast_days": 14
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
+
