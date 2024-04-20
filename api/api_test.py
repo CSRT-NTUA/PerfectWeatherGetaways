cffecaa99638833d8d62cfd858d8ef602d63d430
Spiros Maggioros <spirosastro@gmail.com>
2024-04-20 11:03:54 +0300

    finished api

--- api/api_test.py
+++ api/api_test.py
@@ -19,9 +19,14 @@ def get_answer(prompt:str):
   )
   # get the results of the api, it's in json format 
   response = response["choices"][0]["message"]["content"]
-  json_places = json.loads(response)
-  json_places = json_places['places']
-  return json_places
+  json_result = json.loads(response)
+  json_categories = None
+  json_places = None
+  if json_result.get('categories'):
+    json_categories = json_result['categories']
+  if json_result.get('places'):
+    json_places = json_result['places']
+  return json_places, json_categories
 
 def travelmyth_api(destination, categories):
   url = "https://www.travelmyth.gr/api_chat_makeathon.php"
@@ -43,9 +48,7 @@ def travelmyth_api(destination, categories):
       print("Error:", response.status_code)
 
 prompt = "I want to go for sunny holidays in Europe. I want a city with a big cathedral. Use the following instructions when answering the prompt above: Reply in Json format Include the places suggestions in an array. Suggest as many as you can, preferably at least 10. When writing, the place name includes only the name, not the country, wider region or continent. If the prompt is appropriate for any of the following categories include the category in the json: Categories: infinity_pool,heated_pool,indoor_pool,rooftop_pool,wave_pool,children_pool,panoramic_view_pool,pool_swim_up_bar,pool_water_slide,pool_lap_lanes,water_park,lazy_river,private_pool,dog_play_area,dog_sitting,dogs_stay_free,outdoor_pool,health_and_safety,treehouse,haunted,overwater_bungalows,three_star,skyscraper,four_star,five_star,yoga,tennis,small,adult_only,gym,accessible,cheap,parking,business,free_wifi,pool,nightlife,romantic,dog_friendly,family,spa,casino,honeymoon,eco_friendly,beach,beachfront,ski,ski_in_ski_out,historic,unusual,vineyard,monastery,castle,golf,luxury,boutique,ev_charging,jacuzzi_hot_tub,fireplace,all_inclusive"
-json_places = get_answer(prompt)
+json_places, json_categories = get_answer(prompt)
 print(json_places)
 for x in json_places:
   print(travelmyth_api(x, []))
-#for x in json_data:
-#  print(travelmyth_api(x, []))
