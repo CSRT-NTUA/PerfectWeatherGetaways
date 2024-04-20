5469b4a303d0623cd9965a8545cb0c8d2fe8c509
Spiros Maggioros <spirosastro@gmail.com>
2024-04-20 17:28:02 +0300

    classification and map done

--- data_preprocessing/map_plot.py
+++ data_preprocessing/map_plot.py
@@ -2,16 +2,18 @@ import streamlit as st
 from streamlit_folium import folium_static
 import folium
 
-def create_map(lat, lon):
+def create_map(lat, lon, locations):
     # Set up Streamlit app title
-    st.title("Customized Folium Map in Streamlit")
+    st.title("Destination mapping")
 
     # Create a Folium map object with custom attributes
     m = folium.Map(location=[lat, lon], zoom_start=10, control_scale=True)
-
+    
+    for loc in locations:
+        folium.Marker(location=[loc[0], loc[1]], popup="Recommendation", icon=folium.Icon(icon='exclamation-sign')).add_to(m)
     # Add a marker with a custom icon and popup
-    folium.Marker(location=[lat, lon], popup='Destination', icon=folium.Icon(icon='cloud')).add_to(m)
-
+    folium.Marker(location=[lat, lon], popup='Recommendation', icon=folium.Icon(icon='exclamation-sign')).add_to(m)
+    folium.LayerControl().add_to(m)
     # Add a heatmap layer to the map
     #heat_data = [(51.5, -0.1, 1), (51.55, -0.2, 2), (51.6, -0.15, 3)]
     #folium.plugins.HeatMap(heat_data).add_to(m)
@@ -21,3 +23,7 @@ def create_map(lat, lon):
 
     # Display the map in Streamlit
     folium_static(m)
+
+
+if __name__ == "__main__":
+    create_map(42.43242, 1.4124, [(41.2313, 1.421), (41.3231, '1.4141')])
