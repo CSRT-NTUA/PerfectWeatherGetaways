a98da1a255eff23f5c296de777425238189dcf9b
Ctziapo <44526930+Ctziapo@users.noreply.github.com>
2024-04-20 17:38:06 +0300

    added logo

--- frontend/perfect_weather_gateway_app.py
+++ frontend/perfect_weather_gateway_app.py
@@ -84,11 +84,11 @@ def show_categories():
     selected_others = st.multiselect("Select other options", others)
 
 
-"""def how_it_works():
-    with open("api_test.py", 'r') as file:
-        text = file.read()
-    st.write("This is the api for asking info:")
-    st.code(text, language='python')"""
+#def how_it_works():
+#    with open("api_test.py", 'r') as file:
+#        text = file.read()
+#    st.write("This is the api for asking info:")
+#    st.code(text, language='python')
     
 
 def assign_score(df, weather, min_temperature, max_temperature, date_of_arrival, date_of_deperature):
@@ -130,7 +130,11 @@ def assign_score(df, weather, min_temperature, max_temperature, date_of_arrival,
     return score
 
 def user_input():
-    prompt = st.text_input("Enter: ")
+    st.title("Perfect Weather Gateway")
+    st.image('../assets/images/travelsmyth_logo.png')
+
+
+    prompt = st.text_input("Enter your trip: ")
     st.markdown("<h4 style='text-align: center;'>Add more options for your trip</h4>", unsafe_allow_html=True)
 
     st.markdown("#### Select dates")
@@ -159,26 +163,29 @@ def user_input():
     show_categories()
     
     if st.button('Search'):
-        prompt += "Use the following instructions when answering the prompt above: Reply in Json format Include the places suggestions in an array. Suggest as many as you can, preferably at least 10. When writing, the place name includes only the name, not the country, wider region or continent. If the prompt is appropriate for any of the following categories include the category in the json: Categories: infinity_pool,heated_pool,indoor_pool,rooftop_pool,wave_pool,children_pool,panoramic_view_pool,pool_swim_up_bar,pool_water_slide,pool_lap_lanes,water_park,lazy_river,private_pool,dog_play_area,dog_sitting,dogs_stay_free,outdoor_pool,health_and_safety,treehouse,haunted,overwater_bungalows,three_star,skyscraper,four_star,five_star,yoga,tennis,small,adult_only,gym,accessible,cheap,parking,business,free_wifi,pool,nightlife,romantic,dog_friendly,family,spa,casino,honeymoon,eco_friendly,beach,beachfront,ski,ski_in_ski_out,historic,unusual,vineyard,monastery,castle,golf,luxury,boutique,ev_charging,jacuzzi_hot_tub,fireplace,all_inclusive"
-        results = get_answer(prompt)
-        weathers = []
-        scores = {}
-        points = []
-        for x in results:
-            points.append((x['latitude'], x['longitude']))
-            current_csv = get_weather(x['latitude'], x['longitude'])
-            current_csv = mean_implementation(current_csv)
-            # youre ready to do the classification
-            # user inputs are [weather, min_temp, max_temp, date_of_arrival, date_of_departure, selected_country]
-            scores[x['name']] = assign_score(current_csv, weather, min_temperature, max_temperature, date_of_arrival, date_of_deperature)
-        #st.write(weathers)
-        #st.write(results)
-        scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
-        top_recommendation = next(iter(scores))
-        container = st.container(border=True)
-        for (index, x) in enumerate(scores):
-            container.write(f"{index + 1}'st Recomendation : {x}")
-        create_map(points[0][0], points[0][1], points)
+        with st.spinner(text='In progress'):
+            prompt += "Use the following instructions when answering the prompt above: Reply in Json format Include the places suggestions in an array. Suggest as many as you can, preferably at least 10. When writing, the place name includes only the name, not the country, wider region or continent. If the prompt is appropriate for any of the following categories include the category in the json: Categories: infinity_pool,heated_pool,indoor_pool,rooftop_pool,wave_pool,children_pool,panoramic_view_pool,pool_swim_up_bar,pool_water_slide,pool_lap_lanes,water_park,lazy_river,private_pool,dog_play_area,dog_sitting,dogs_stay_free,outdoor_pool,health_and_safety,treehouse,haunted,overwater_bungalows,three_star,skyscraper,four_star,five_star,yoga,tennis,small,adult_only,gym,accessible,cheap,parking,business,free_wifi,pool,nightlife,romantic,dog_friendly,family,spa,casino,honeymoon,eco_friendly,beach,beachfront,ski,ski_in_ski_out,historic,unusual,vineyard,monastery,castle,golf,luxury,boutique,ev_charging,jacuzzi_hot_tub,fireplace,all_inclusive"
+            results = get_answer(prompt)
+            weathers = []
+            scores = {}
+            points = []
+            for x in results:
+                points.append((x['latitude'], x['longitude']))
+                current_csv = get_weather(x['latitude'], x['longitude'])
+                current_csv = mean_implementation(current_csv)
+                # youre ready to do the classification
+                # user inputs are [weather, min_temp, max_temp, date_of_arrival, date_of_departure, selected_country]
+                scores[x['name']] = assign_score(current_csv, weather, min_temperature, max_temperature, date_of_arrival, date_of_deperature)
+            #st.write(weathers)
+            #st.write(results)
+            scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
+            top_recommendation = next(iter(scores))
+            container = st.container(border=True)
+            for (index, x) in enumerate(scores):
+                container.write(f"{index + 1}'st Recomendation : {x}")
+            create_map(points[0][0], points[0][1], points)
+            
+            st.success('Done')
         
 if __name__ == "__main__":
     st.sidebar.header("Menu")
@@ -191,8 +198,8 @@ if __name__ == "__main__":
     if st.sidebar.button('User input'):
         st.session_state.screen = "user_input"
         
-    """if st.sidebar.button('How it works?'):
-        st.session_state.screen = "how_it_works" """
+    #if st.sidebar.button('How it works?'):
+    #    st.session_state.screen = "how_it_works" 
         
         
     if st.session_state.screen == "welcome_screen":
@@ -201,8 +208,8 @@ if __name__ == "__main__":
         show_results()
     if st.session_state.screen == "user_input":
         user_input()
-    """if st.session_state.screen == "how_it_works":
-        how_it_works() """
+    #if st.session_state.screen == "how_it_works":
+    #    how_it_works()
     
     #data = pd.read_csv("../api/data.csv")
     #fig = plot_weather_data(data, )
